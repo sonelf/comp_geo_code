b665841f7e3e9717a3b64c4914b298abc5fa433b
sfucci350 <sfucci@brynmawr.edu>
2019-02-23 12:43:31 -0500

    first commit of hw

---
+++ GrahamScan/GrahamScan.java
@@ -0,0 +1,32 @@
+public class GrahamScan {
+  public static void main(String[] args){
+
+    Point [] points = new Point[10];
+    Point br = getBottomRight(points);
+
+  }
+
+  public static Point getBottomRight(Point[] points){
+    Array of points [points]
+    Point br = points[0]; //bottommost right
+    brX = br.getX();
+    brY = br.getY();
+
+    for(int i = 1; i <points.length;i++){
+      Point ptTmp = points.get(i);
+      Point ptTmpX = ptTmp.getX();
+      Point ptTmpY = ptTmp.getY();
+
+      if(ptTmpX > brX && ptTmpY < brY){
+        br = ptTmp;
+        brX = ptTmpX;
+        brY = ptTmpY;
+      }
+    }
+
+  }
+
+
+
+
+}
